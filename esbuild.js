659b9126c4862763064002f6a1caf8d0639a1d42
Valexr <valexr@gmail.com>
2021-08-20 14:52:12 +0300

    first commit

---
+++ esbuild.js
@@ -0,0 +1,56 @@
+import { build } from "esbuild";
+import { derver } from "derver";
+import sveltePlugin from "esbuild-svelte";
+import sveltePreprocess from "svelte-preprocess";
+import gloryPreprocess from "glory-svelte-preprocess";
+
+const DEV = process.argv.includes('--dev');
+
+// Development server configuration. To configure production server
+// see `start` script in `package.json` file.
+
+const HOST = 'localhost';
+const PORT = 5050;
+
+build({
+    // esbuild configuration
+    entryPoints: ['src/main.js'],
+    bundle: true,
+    outfile: 'public/build/bundle.js',
+    mainFields: ['svelte', 'module', 'main'],
+    minify: !DEV,
+    incremental: DEV,
+    sourcemap: DEV,  // Use `DEV && 'inline'` to inline sourcemaps to the bundle
+    plugins: [
+        sveltePlugin({
+
+            compileOptions: {
+                // Svelte compile options
+                dev: DEV,
+                css: false  //use `css:true` to inline CSS in `bundle.js`
+            },
+
+            preprocess: [
+                // Place here any Svelte preprocessors
+                sveltePreprocess(),
+                gloryPreprocess(),
+            ]
+
+        })
+    ]
+
+}).then(bundle => {
+    DEV && derver({
+        dir: 'public',
+        host: HOST,
+        port: PORT,
+        watch: ['public', 'src'],
+        onwatch: async (lr, item) => {
+            if (item == 'src') {
+                lr.prevent();
+                bundle.rebuild().catch(err => lr.error(err.message, 'Svelte compile error'));
+            }
+        }
+    })
+});
+
